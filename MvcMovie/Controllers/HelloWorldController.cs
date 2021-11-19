904693ada8c58c39a91b59e7a682f826d8b8696c
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-11-19 09:53:15 -0300

    Criado projeto e adicionado controlador HelloWorld e acoes Index e Welcome deste para teste

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class c : Controller
+    {
+        public string Index()
+        {
+            //return View();
+            return "Esta é minha action padrão!";
+        }
+
+        public string Welcome()
+        {
+            return "Esta é a ação bem-vindo!";
+        }
+    }
+}
