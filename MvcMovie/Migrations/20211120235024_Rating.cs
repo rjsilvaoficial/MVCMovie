b4a34e17e0e94713e3f200358ed56d0bc1814a08
rjsilvaoficial <rjsilvaoficial@gmail.com>
2021-11-20 21:21:53 -0300

    Adicionado campo de busca por genero e palavra chave

---
+++ MvcMovie/Migrations/20211120235024_Rating.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider servicesProvider)
+        {
+            using (var seedContext = new MvcMovieContext
+                (servicesProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                if (seedContext.Movie.Any())
+                {
+                    return;
+                }
+
+                seedContext.AddRange
+                    (
+                        new Movie { Title = "Duro de matar 1", ReleaseDate = DateTime.Parse("1984-3-13"), Genre = "Ação", Price = 1.99M, Rating = "R" },
+                        new Movie { Title = "Duro de matar 1", ReleaseDate = DateTime.Parse("1985-4-14"), Genre = "Ação", Price = 2.99M, Rating = "R" },
+                        new Movie { Title = "Duro de matar 1", ReleaseDate = DateTime.Parse("1986-5-15"), Genre = "Ação", Price = 3.99M, Rating = "R" },
+
+                        new Movie { Title = "Predador 1", ReleaseDate = DateTime.Parse("1987-6-16"), Genre = "Ação", Price = 1.99M, Rating = "R" },
+                        new Movie { Title = "Predador 2", ReleaseDate = DateTime.Parse("1988-7-17"), Genre = "Ação", Price = 2.99M, Rating = "R" }
+                    );
+                seedContext.SaveChanges();
+            }
+        }
+    }
+}
